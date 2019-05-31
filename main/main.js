d6df0ef4d5a82947af99c1272b94faf150406a97
Yijiehuahua <345239814@qq.com>
2019-05-31 15:02:02 +0800

    finally done!

--- main/main.js
+++ main/main.js
@@ -1,36 +1,29 @@
-"use strict";
-
-module.exports = function main(input) {
-	let numbers = `
+module.exports = function main(a) {
+let numbers = `
 ._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
 |.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
 |_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
 `;
-
-	/* 将字符串转化为数组保存，按数字牵引保存 */
-	let splitArr = numbers.split(/   |\n/),
-		numberArr = [],
-		k=0;
-	for(let i=0,l=splitArr.length;i<l;i++){
-		let numberStr = splitArr[i];
-		if(''==numberStr) continue;
-		if(10==k) k=0; //十组一循环
-		if(!(k in numberArr))
-			numberArr[k] = []; //二维数组未初始化
-		numberArr[k].push(numberStr);
-		k++;
+let splitArr = numbers.split(/   |\n/),
+		number = [],
+		j=0;
+for(let i=0,l=splitArr.length;i<l;i++){
+	let numberStr = splitArr[i];
+	if(''==numberStr) continue;
+	if(10==j) j=0;
+	if(!(j in number))
+		number[j] = [];
+	  number[j].push(numberStr);
+		j++;
+}
+let b = '';
+for(let i=0;i<3;i++){
+	for(let j=0,l=a.length;j<l;j++){
+		let numberr = a[j];
+		b += number[numberr][i];
+		if(j!=l-1) b += " ";
 	}
-	console.log(numberArr);
-
-	let output = '';
-	for(let i=0;i<3;i++){
-		for(let k=0,l=input.length;k<l;k++){
-			let number = input[k];
-			output += numberArr[number][i];
-			if(k!=l-1) output += " ";
-		}
-		output += "\n";
-	}
-    console.log(output);
-	return output;
+	b += "\n";
+}
+return b;
 };
